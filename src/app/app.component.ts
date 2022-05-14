b0fd41b2944431cdc1336f979fafd18a73a917ef
sloregit <s.lorenzoni2@studenti.unipi.it>
2022-05-14 17:53:47 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component, VERSION } from '@angular/core';
-import {TeatroDBService} from ''
+import { TeatroDBService } from './teatro-db.service';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
 }
