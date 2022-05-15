5449d9bccb553401661fbf40e8bd482c9e754f0a
sloregit <s.lorenzoni2@studenti.unipi.it>
2022-05-15 17:13:09 +0200

    1 file modified

--- src/app/teatro/pulsante/pulsante.component.ts
+++ src/app/teatro/pulsante/pulsante.component.ts
@@ -10,7 +10,7 @@ export class PulsanteComponent implements OnInit {
   etichetta: Node;
   value: string;
   aCapo: HTMLElement;
-  parent: HTMLElement;
+  parent: HTMLElement = document.getElementById('pulsante');
   mostraNome() {
     try {
       //@Output val = this.value;
@@ -30,18 +30,17 @@ export class PulsanteComponent implements OnInit {
       console.error('errore in: Pulsante.mostraNome', e.message, e.name);
     }
   }
-  constructor(nome: string, LFila: number, posto: number, parent) {
+  constructor() {
     try {
       this.pulsante = document.createElement('button');
-      this.etichetta = document.createTextNode('');
-      this.etichetta.textContent = 'P' + (posto + 1);
+      this.etichetta = document.createTextNode('P');
+      this.etichetta.textContent = 'P'; //+ (posto + 1);
       this.pulsante.appendChild(this.etichetta);
       this.aCapo = document.createElement('br');
-      this.parent = parent;
-      parent.appendChild(this.pulsante);
-      posto + 1 >= LFila ? parent.appendChild(this.aCapo) : '';
-      this.pulsante.value = nome != undefined ? nome : 'x';
-      this.pulsante.className = nome != 'x' ? 'prenotato' : 'libero';
+      this.parent.appendChild(this.pulsante);
+      //posto + 1 >= LFila ? parent.appendChild(this.aCapo) : '';
+      //this.pulsante.value = nome != undefined ? nome : 'x';
+      //this.pulsante.className = nome != 'x' ? 'prenotato' : 'libero';
       this.pulsante.addEventListener('click', this.mostraNome);
     } catch (e: any) {
       console.error('errore in: Pulsante(constructor)', e.message, e.name);
