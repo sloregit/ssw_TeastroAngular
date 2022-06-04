0cdb16d419d0ab00c11f4db68fe764becef176f3
sloregit <s.lorenzoni2@studenti.unipi.it>
2022-06-04 17:44:27 +0200

    3 files modified

--- src/app/teatro/pulsante/pulsante.component.ts
+++ src/app/teatro/pulsante/pulsante.component.ts
@@ -7,25 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PulsanteComponent implements OnInit {
   pulsante;
-  etichetta: Node;
+  etichetta: string;
   value: string;
   aCapo: HTMLElement;
   parent: HTMLElement = document.getElementById('pulsante');
   mostraNome() {
     try {
-      //@Output val = this.value;
-      /*
-      if (Selezionato(this)) {
-        //se il posto è libero
-        if (this.value == 'x') {
-          Postolibero = true;
-        } else if (this.value != 'x') {
-          Postolibero = false;
-          parNomi.innerHTML = 'Il posto è gia prenotato';
-          //alert('il posto è già prenotato');
-        }
-        parNomi.innerHTML = this.value;}
-        */
     } catch (e: any) {
       console.error('errore in: Pulsante.mostraNome', e.message, e.name);
     }
@@ -33,14 +20,7 @@ export class PulsanteComponent implements OnInit {
   constructor() {
     try {
       this.pulsante = document.createElement('button');
-      this.etichetta = document.createTextNode('P');
-      this.etichetta.textContent = 'P'; //+ (posto + 1);
-      this.pulsante.appendChild(this.etichetta);
-      this.aCapo = document.createElement('br');
-      this.parent.appendChild(this.pulsante);
-      //posto + 1 >= LFila ? parent.appendChild(this.aCapo) : '';
-      //this.pulsante.value = nome != undefined ? nome : 'x';
-      //this.pulsante.className = nome != 'x' ? 'prenotato' : 'libero';
+      this.etichetta = 'P';
       this.pulsante.addEventListener('click', this.mostraNome);
     } catch (e: any) {
       console.error('errore in: Pulsante(constructor)', e.message, e.name);
