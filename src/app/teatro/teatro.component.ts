9c64e2ff9970c5221ad950e441b9202bf27c3ef0
sloregit <s.lorenzoni2@studenti.unipi.it>
2022-06-07 19:16:33 +0200

    2 files modified

--- src/app/teatro/teatro.component.ts
+++ src/app/teatro/teatro.component.ts
@@ -1,21 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 import { PulsanteComponent } from './pulsante/pulsante.component';
 import { TeatroDBService } from '../teatro-db.service';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-teatro',
   templateUrl: './teatro.component.html',
   styleUrls: ['./teatro.component.css'],
 })
 export class TeatroComponent implements OnInit {
-  a = new PulsanteComponent();
+  //a = new PulsanteComponent();
   prenotazioni;
   constructor(private TeatroDBservice: TeatroDBService) {}
+  filePlatea: number;
+  postiPlatea: number;
+  filePalco: number;
+  postiPalco: number;
+  platea: Array<Array<string>>;
+  palco: Array<Array<string>>;
   getTeatro() {
-    this.prenotazioni = this.TeatroDBservice.getPrenotazioni$().subscribe(
-      (val) => console.log(val)
-    );
+    this.TeatroDBservice.getPrenotazioni$().subscribe({
+      next: (res) => {
+        //res è una stringa => trasformo in Json
+        this.prenotazioni = JSON.parse(res);
+        console.log(this.prenotazioni);
+        this.filePlatea = this.prenotazioni.platea.length;
+        this.postiPlatea = this.prenotazioni.platea[0].length;
+        this.filePalco = this.prenotazioni.palco.length;
+        this.postiPalco = this.prenotazioni.palco[0].length;
+        this.platea = this.prenotazioni.platea.map((fila: Array<string>) =>
+          fila.map((val: string, posto: number) =>
+            //new Pulsante(val, this.postiPlatea, posto, 'platea')
+            console.log('platea: ' + val, posto)
+          )
+        );
+        this.palco = this.prenotazioni.palco.map((fila: Array<string>) =>
+          fila.map((val: string, posto: number) =>
+            //new Pulsante(val, this.postiPalco, posto, 'palco')
+            console.log('palco: ' + val, posto)
+          )
+        );
+      },
+    });
   }
-  ngOnInit() {}
+  ngOnInit;
 }
 
 ///sottoscrizione ai dati fatta
