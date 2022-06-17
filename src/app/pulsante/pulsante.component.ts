912ed44e6601162e8887b9fb6a0a79070e5fef21
sloregit <s.lorenzoni2@studenti.unipi.it>
2022-06-17 18:48:55 +0200

    1 file modified

--- src/app/pulsante/pulsante.component.ts
+++ src/app/pulsante/pulsante.component.ts
@@ -20,7 +20,10 @@ export class PulsanteComponent implements OnInit {
   //invia al parent i dati del pulsante cliccato(this)
   mostraNome() {
     try {
-      this.evidenzia = true;
+      if (this.evidenzia == true) {
+        this.evidenzia = false;
+      }
+
       this.mostraNomeEmitter.emit(this);
     } catch (e: any) {
       console.error('errore in: Pulsante.mostraNome', e.message, e.name);
@@ -29,7 +32,6 @@ export class PulsanteComponent implements OnInit {
   //modifica lo stile dell'ultimo pulsante cliccato e dei posti prenotati
   stile() {
     if (this.evidenzia == true) {
-      this.evidenzia = false;
       return { 'background-color': 'green' };
     }
     return { 'background-color': this.nomePosto != 'x' ? 'red' : 'turquoise' };
