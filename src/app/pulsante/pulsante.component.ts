28b189f4c24f20b6a1bcd3d9812045069cef4253
sloregit <s.lorenzoni2@studenti.unipi.it>
2022-06-18 15:28:41 +0200

    3 files modified

--- src/app/pulsante/pulsante.component.ts
+++ src/app/pulsante/pulsante.component.ts
@@ -16,7 +16,7 @@ export class PulsanteComponent implements OnInit {
   @Input() etichetta: string;
   @Input() nomePosto: string;
   @Output() mostraNomeEmitter = new EventEmitter<object>();
-  evidenzia: boolean;
+  evidenzia: boolean = false;
   selezionati = [];
   a;
   //invia al parent i dati del pulsante cliccato(this)
