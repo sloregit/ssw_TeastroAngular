913bc0ec1d6d2e3d131523a9955f9990bcc455ec
sloregit <s.lorenzoni2@studenti.unipi.it>
2022-06-21 15:36:42 +0200

    1 file modified, 5 added, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,17 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { AppDBService } from './app-db.service';
 import { TeatroComponent } from './teatro/teatro.component';
-import { PulsanteComponent } from './pulsante/pulsante.component';
+import { PulsanteComponent } from './teatro/sala-teatro/pulsante/pulsante.component';
+import { SalaTeatroComponent } from './teatro/sala-teatro/sala-teatro.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, HttpClientModule],
-  declarations: [AppComponent, PulsanteComponent, TeatroComponent],
+  declarations: [
+    AppComponent,
+    PulsanteComponent,
+    TeatroComponent,
+    SalaTeatroComponent,
+  ],
   bootstrap: [AppComponent],
   providers: [AppDBService],
 })
---
+++ src/app/teatro/sala-teatro/sala-teatro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sala-teatro',
+  templateUrl: './sala-teatro.component.html',
+  styleUrls: ['./sala-teatro.component.css']
+})
+export class SalaTeatroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
